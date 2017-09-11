eef97a6875dc01ccce6835dcca28bd270e6c5586
Vince Dang <xuanvu001@e.ntu.edu.sg>
2017-09-12 02:17:28 +0800

    remove SimpleSchema dependencies

--- imports/api/notifications/notifications.js
+++ imports/api/notifications/notifications.js
@@ -5,29 +5,7 @@
 
 import {Mongo} from 'meteor/mongo'
 
-import {SimpleSchema} from 'meteor/aldeed:simple-schema'
-
 export const Notifications = new Mongo.Collection('notifications')
-
-Notifications.schema = new SimpleSchema({
-  time: {
-    type: String
-  },
-  title: {
-    type: String
-  },
-  category: {
-    type: [Object]
-  },
-  'category.label': {
-    type: String
-  },
-  'category.icon': {
-    type: String
-  }
-})
-
-Notifications.attachSchema(Notifications.schema)
 
 Notifications.publicFields = {
   time: 1,
--- imports/api/tasks/tasks.js
+++ imports/api/tasks/tasks.js
@@ -5,23 +5,7 @@
 
 import {Mongo} from 'meteor/mongo'
 
-import {SimpleSchema} from 'meteor/aldeed:simple-schema'
-
 export const Tasks = new Mongo.Collection('tasks')
-
-Tasks.schema = new SimpleSchema({
-  title: {
-    type: String
-  },
-  progress: {
-    type: Number
-  },
-  label: {
-    type: String
-  }
-})
-
-Tasks.attachSchema(Tasks.schema)
 
 Tasks.publicFields = {
   title: 1,
