3a7b8ad2a772f4438b7b0c98c886ff319d2a5b17
Vince Dang <xuanvu001@e.ntu.edu.sg>
2017-09-29 00:25:31 +0800

    add error message for profile

--- imports/ui/pages/team-task.js
+++ imports/ui/pages/team-task.js
@@ -1,22 +1,18 @@
-import {Tasks} from '../../api/tasks/tasks'
-import {Links} from '../../api/links/links'
+import { Tasks } from '../../api/tasks/tasks'
+import { Links } from '../../api/links/links'
 
 import './team-task.html'
 import '../stylesheets/gantt.css'
 import '../stylesheets/team-task.css'
 
-Template
-  .teamTask
-  .onCreated(() => {
-    Meteor.subscribe('tasks')
-    Meteor.subscribe('links')
-  })
+Template.teamTask.onCreated(() => {
+  Meteor.subscribe('tasks')
+  Meteor.subscribe('links')
+})
 
 Template.teamTask.rendered = function () {
   var daysStyle = function (date) {
-    var dateToStr = gantt
-      .date
-      .date_to_str("%D");
+    var dateToStr = gantt.date.date_to_str("%D");
     if (dateToStr(date) == "Sun" || dateToStr(date) == "Sat") {
       return "weekend";
     }
@@ -103,18 +99,12 @@ Template.teamTask.rendered = function () {
     }
   ];
   gantt.attachEvent("onLightboxButton", function (button_id, node, e) {
-    var id = gantt
-      .getState()
-      .lightbox;
+    var id = gantt.getState().lightbox;
     if (button_id == "completed_button") {
-      gantt
-        .getTask(id)
-        .progress = 1;
+      gantt.getTask(id).progress = 1;
       //To send sms
     } else if (button_id == "reviewed_button") {
-      gantt
-        .getTask(id)
-        .progress = 2;
+      gantt.getTask(id).progress = 2;
     }
     gantt.updateTask(id);
     gantt.hideLightbox();
@@ -122,34 +112,28 @@ Template.teamTask.rendered = function () {
   gantt.locale.labels.section_template = "Details";
 
   // Init dhtmlxGantt data adapter.
-  gantt.meteor({tasks: Tasks, links: Links});
+  gantt.meteor({ tasks: Tasks, links: Links });
 }
 
-Template
-  .teamTask
-  .events({
-    'focus #date-start': function (e) {
-      e.preventDefault()
-      var dateStart = $('#date-start')
-      dateStart.bootstrapMaterialDatePicker({format: 'YYYY-MM-DD', time: false})
-    },
-    'focus #date-end': function (e) {
-      e.preventDefault()
-      var dateEnd = $('#date-end')
-      dateEnd.bootstrapMaterialDatePicker({format: 'YYYY-MM-DD', time: false})
-    },
-    'change #date-start': function (e, template) {
-      var dateStart = $('[name="date-start"]')
-        .val()
-        .split("-")
-      gantt.config.start_date = new Date(dateStart[0], dateStart[1] - 1, dateStart[2])
-      gantt.render()
-    },
-    'change #date-end': function (e, template) {
-      var dateEnd = $('[name="date-end"]')
-        .val()
-        .split("-")
-      gantt.config.end_date = new Date(dateEnd[0], dateEnd[1] - 1, dateEnd[2])
-      gantt.render()
-    }
-  })
+Template.teamTask.events({
+  'focus #date-start': function (e) {
+    e.preventDefault()
+    var dateStart = $('#date-start')
+    dateStart.bootstrapMaterialDatePicker({ format: 'YYYY-MM-DD', time: false })
+  },
+  'focus #date-end': function (e) {
+    e.preventDefault()
+    var dateEnd = $('#date-end')
+    dateEnd.bootstrapMaterialDatePicker({ format: 'YYYY-MM-DD', time: false })
+  },
+  'change #date-start': function (e, template) {
+    var dateStart = $('[name="date-start"]').val().split("-")
+    gantt.config.start_date = new Date(dateStart[0], dateStart[1] - 1, dateStart[2])
+    gantt.render()
+  },
+  'change #date-end': function (e, template) {
+    var dateEnd = $('[name="date-end"]').val().split("-")
+    gantt.config.end_date = new Date(dateEnd[0], dateEnd[1] - 1, dateEnd[2])
+    gantt.render()
+  }
+})
--- imports/ui/pages/user-profile.js
+++ imports/ui/pages/user-profile.js
@@ -1,92 +1,117 @@
-import {Template} from 'meteor/templating'
+import { Template } from 'meteor/templating'
+import { ReactiveDict } from 'meteor/reactive-dict'
 
-import {InputValidator} from '../../utils/input-validator'
+import { InputValidator } from '../../utils/input-validator'
 
 import './user-profile.html'
 
-Template
-  .userProfile
-  .helpers({
-    isChecked(value) {
-      return value === Meteor
-        .user()
-        .notificationPreference
-        ? "checked"
-        : ""
-    },
-    isChosen(value) {
-      return value === Meteor.user().notificationPreference
-    },
-    username() {
-      return Meteor
-        .user()
-        .emails[0]
-        .address
-    },
-    name() {
-      return Meteor
-        .user()
-        .name
-    },
-    phone() {
-      return Meteor
-        .user()
-        .phone
+Template.userProfile.onCreated(function () {
+  this.state = new ReactiveDict()
+})
+
+Template.userProfile.helpers({
+  isChecked(value) {
+    return value === Meteor.user().notificationPreference ? "checked" : ""
+  },
+  isChosen(value) {
+    return value === Meteor.user().notificationPreference
+  },
+  username() {
+    return Meteor.user().emails[0].address
+  },
+  name() {
+    return Meteor.user().name
+  },
+  phone() {
+    return Meteor.user().phone
+  },
+  piError() {
+    const instance = Template.instance()
+    return instance.state.get('pi-error')
+  },
+  pwError() {
+    const instance = Template.instance()
+    return instance.state.get('pw-error')
+  },
+  piSuccess() {
+    const instance = Template.instance()
+    return instance.state.get('pi-success') ? 'alert-success' : 'alert-danger'
+  },
+  pwSuccess() {
+    const instance = Template.instance()
+    return instance.state.get('pw-success') ? 'alert-success' : 'alert-danger'
+  }
+})
+
+Template.userProfile.events({
+  'change #phone'(e) {
+    e.preventDefault()
+    const phone = $('#phone').val()
+    let message
+    if (!InputValidator.isValidPhone(phone)) {
+      message = 'Invalid Phone Number'
     }
-  })
-
-Template
-  .userProfile
-  .events({
-    'change #phone' (e) {
-      e.preventDefault()
-      const phone = $('#phone').val()
-      if (!InputValidator.isValidPhone(phone)) {
-        console.log('invalid phone')
-      }
-    },
-    'click #personal-info-submit' (e) {
-      e.preventDefault()
-      const phone = $('#phone').val()
-      if (phone && !InputValidator.isValidPhone(phone)) {
-        console.log('invalid phone')
-        return
-      }
-      const name = $('#name').val()
-      console.log(phone + "-" + name)
-
-      Meteor.call('users.updatePersonalInfo', Meteor.userId(), name, phone)
-      $('#phone').val('')
-      $('#name').val('')
-    },
-    'click #pw-submit' (e) {
-      e.preventDefault()
-      const pwCurrent = $('#pw-current').val()
-      if (!pwCurrent) {
-        return
-      }
-      const pwNew = $('#pw-new').val()
-      const pwVerify = $('#pw-verify').val()
-      if (pwNew === pwVerify) {
-        Accounts.changePassword(pwCurrent, pwNew, (e) => {
-          if (e) {
-            console.log(e.reason)
-          } else {
-            console.log("Change password successful")
-          }
-        })
-      } else {
-        console.log('New password does not match')
-      }
-      $('#pw-current').val('')
-      $('#pw-new').val('')
-      $('#pw-verify').val('')
-    },
-    'click #np-submit' (e) {
-      e.preventDefault()
-      const pref = $('input[name="notification-preference"]:checked').val()
-      console.log(pref)
-
-      Meteor.call('users.updateNotificationPreference', Meteor.userId(), pref)
+    const instance = Template.instance()
+    instance.state.set('pi-error', message)
+  },
+  'submit #personal-info-submit'(e) {
+    e.preventDefault()
+    const phone = $('#phone').val()
+    let message
+    let success
+    if (phone && !InputValidator.isValidPhone(phone)) {
+      message = 'Invalid Phone Number'
+      return false
+    } else {
+      message = 'Personal Info Changed Successfully'
+      success = true
     }
-  })
+    const instance = Template.instance()
+    instance.state.set('pi-error', message)
+    instance.state.set('pi-success', success)
+    const name = $('#name').val()
+    console.log(phone + "-" + name)
+
+    Meteor.call('users.updatePersonalInfo', Meteor.userId(), name, phone)
+    $('#phone').val('')
+    $('#name').val('')
+  },
+  'click #pw-submit'(e) {
+    e.preventDefault()
+    const pwCurrent = $('#pw-current').val()
+    if (!pwCurrent) {
+      return
+    }
+    const pwNew = $('#pw-new').val()
+    const pwVerify = $('#pw-verify').val()
+    let message
+    let success
+    const instance = Template.instance()
+    if (pwNew === pwVerify) {
+      Accounts.changePassword(pwCurrent, pwNew, (e) => {
+        if (e) {
+          message = e.reason
+        } else {
+          message = "Change password successful"
+          success = true
+        }
+        instance.state.set('pw-error', message)
+        instance.state.set('pw-success', success)
+      })
+    } else {
+      message = 'New Password Does Not Match'
+    }
+    instance.state.set('pw-error', message)
+    instance.state.set('pw-success', success)
+    $('#pw-current').val('')
+    $('#pw-new').val('')
+    $('#pw-verify').val('')
+  },
+  'click #np-submit'(e) {
+    e.preventDefault()
+    const pref = $('input[name="notification-preference"]:checked').val()
+    console.log(pref)
+
+    Meteor.call('users.updateNotificationPreference', Meteor.userId(), pref)
+  }
+})
