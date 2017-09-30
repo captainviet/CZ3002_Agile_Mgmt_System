9b6c4380eca7658b159fbfec5d6c39bef8940c5d
Desmond <sohskd@hotmail.com>
2017-09-30 17:52:15 +0800

    working display of personal task

--- imports/ui/pages/personal-task.js
+++ imports/ui/pages/personal-task.js
@@ -1,1 +1,25 @@
+import {Template} from 'meteor/templating'
+import {Tasks} from '../../api/tasks/tasks'
+
 import './personal-task.html'
+
+Template
+  .personalTask
+  .onCreated(() => {
+    Meteor.subscribe('tasks')
+  })
+
+Template
+  .personalTask
+  .helpers({
+    displayAllTasks() {
+      counter = 1
+      return Tasks.find({}).map(function(item) {
+        return {
+          task_text: item.text,
+          task_priority: item.priority,
+          counter: counter++
+        }
+      })
+    }
+  })
