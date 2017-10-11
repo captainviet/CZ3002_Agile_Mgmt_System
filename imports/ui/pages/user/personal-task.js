51721d652857773d2841248e4cc649c5a0519ea3
Desmond <sohskd@hotmail.com>
2017-10-11 11:29:53 +0800

    Personal task page

--- imports/ui/pages/user/personal-task.js
+++ imports/ui/pages/user/personal-task.js
@@ -13,13 +13,43 @@ Template
   .personalTask
   .helpers({
     displayAllTasks() {
-      counter = 1
-      return Tasks.find({}).map(function(item) {
+      var monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+      ];
+      counter = 1;
+
+      return Tasks.find({'priority':Meteor.user().name},
+                        {sort: {'duration': 1}}).map(function(item) {
+        var task_progress;
+        task_progress = (item.progress === 1) ? 'Completed' :
+                        (item.progress === 2) ? 'Reviewed' : 'Pending';
         return {
           task_text: item.text,
           task_priority: item.priority,
+          task_start_date: item.start_date.getDate() + ' ' + monthNames[item.start_date.getMonth()] + ' ' + item.start_date.getFullYear(),
+          task_end_date: item.end_date.getDate() + ' ' + monthNames[item.end_date.getMonth()] + ' ' + item.end_date.getFullYear(),
+          task_duration: item.duration,
+          task_progress: task_progress,
           counter: counter++
         }
       })
     }
   })
+
+  Template
+  .personalTask
+  .events({
+    'click .startDate' (e) {
+      e.preventDefault()
+      console.log("clicked startDate!");
+    }
+  })
+
+  Template.registerHelper('myTasksFunction', () => {
+
+    let tasks = Tasks.find({'priority':Meteor.user().name},
+                      {sort: {'duration': 1}});
+
+    console.log(tasks[0]);
+    return tasks;
+  });
