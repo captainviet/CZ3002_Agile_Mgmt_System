3acd914e9d9f1ef1b83f32b18c6e9ed8b64ee0c2
Vince Dang <xuanvu001@e.ntu.edu.sg>
2017-10-12 14:07:43 +0800

    Integrate personal task to application

--- imports/ui/pages/user/personal-task.js
+++ imports/ui/pages/user/personal-task.js
@@ -1,31 +1,37 @@
-import {Template} from 'meteor/templating'
-import {Tasks} from '../../api/tasks/tasks'
+import { Template } from 'meteor/templating'
+import { Tasks } from '../../../api/tasks/tasks'
 
 import './personal-task.html'
 
-Template
-  .personalTask
-  .onCreated(() => {
-    Meteor.subscribe('tasks')
-  })
+Template.personalTask.onCreated(() => {
+  Meteor.subscribe('tasks')
+  Meteor.subscribe('users')
+})
 
-Template
-  .personalTask
-  .helpers({
-    displayAllTasks() {
-      var monthNames = ["January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"
-      ];
-      counter = 1;
+Template.personalTask.helpers({
+  displayAllTasks() {
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+    ];
+    counter = 1;
 
-      return Tasks.find({'priority':Meteor.user().name},
-                        {sort: {'duration': 1}}).map(function(item) {
-        var task_progress;
-        task_progress = (item.progress === 1) ? 'Completed' :
-                        (item.progress === 2) ? 'Reviewed' : 'Pending';
+    return Tasks.find({ 'assignee': Meteor.userId() },
+      { sort: { 'end_date': 1 } }).map(function (item) {
+        let task_progress
+        switch (item.progress) {
+          case 1:
+            task_progress = 'Completed'
+            break;
+          case 2:
+            task_progress = 'Reviewed'
+          default:
+            task_progress = 'Pending'
+        }
+        const user = Meteor.users.findOne(item.assignee)
+        const name = user.name ? user.name : user.emails[0].address
         return {
           task_text: item.text,
-          task_priority: item.priority,
+          task_priority: name,
           task_start_date: item.start_date.getDate() + ' ' + monthNames[item.start_date.getMonth()] + ' ' + item.start_date.getFullYear(),
           task_end_date: item.end_date.getDate() + ' ' + monthNames[item.end_date.getMonth()] + ' ' + item.end_date.getFullYear(),
           task_duration: item.duration,
@@ -33,23 +39,5 @@ Template
           counter: counter++
         }
       })
-    }
-  })
-
-  Template
-  .personalTask
-  .events({
-    'click .startDate' (e) {
-      e.preventDefault()
-      console.log("clicked startDate!");
-    }
-  })
-
-  Template.registerHelper('myTasksFunction', () => {
-
-    let tasks = Tasks.find({'priority':Meteor.user().name},
-                      {sort: {'duration': 1}});
-
-    console.log(tasks[0]);
-    return tasks;
-  });
+  }
+})
